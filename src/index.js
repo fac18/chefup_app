4809efbe0d8225613017f47fdfeb8c0828645876
Maria <m.olanipekun@hotmail.com>
2020-01-22 16:26:39 +0000

    set up react routing closes #53

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Route, Link, BrowserRouter as router, Router } from 'react-router-dom'
 import "./index.css";
 import App from "./App";
+import Header from './components/Header/Header'
 import * as serviceWorker from "./serviceWorker";
 import "typeface-roboto";
 import "typeface-roboto-mono";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const routing = (
+    <Router>
+        <div>
+            <Route exact path='/' component={App} />
+            <Route path='/createRecipe' component={createRecipe} />
+            <Route path='/displayRecipe' component={displayRecipe} />
+        </div>
+    </Router>
+)
+
+ReactDOM.render(routing, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
