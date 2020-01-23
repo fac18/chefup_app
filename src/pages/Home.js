2de22ee8e102a6ad9dea27a9829d7f25b48e7b69
Maria <m.olanipekun@hotmail.com>
2020-01-23 11:04:05 +0000

    delete logo.svg in root, delete logo import

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer/Footer'
-import Logo from "../components/Logo/Logo";
 
 const Home = () => {
     return (
         <div>
-            <Header><Logo /> </Header>
+            <Header> </Header>
             <Footer />
         </div>
     )
