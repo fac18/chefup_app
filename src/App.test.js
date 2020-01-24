9e16e214747cc51800c9410898d2e9373379a479
Maria <m.olanipekun@hotmail.com>
2020-01-24 11:25:36 +0000

    added withRouter to fix testing issues and working on app.js test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
+import { Router, withRouter } from 'react-router-dom'
+import { createMemoryHistory } from 'history'
+import { render, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('full app rendering/navigating', () => {
+  const history = createMemoryHistory()
+  const { container, getByText } = render(
+    <Router history={withRouter(history)}>
+      <App />
+    </Router>
+  )
+  expect(container.textContent).toMatch('About')
+
+  fireEvent.click(getByText(/about/i))
+
+  // check that the content changed to the new page
+  expect(container.innerHTML).toMatch('You are on the about page')
+})
--- src/pages/aboutUs.js
+++ src/pages/aboutUs.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Header from '../components/Header/Header'
 
 const aboutUs = () => {
     return (
         <div>
-            <p>All about us</p>
+            <Header />
+            <h3>About Us</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Commodo, fusce platea cras imperdiet tortor et. Pellentesque vitae sed molestie nunc cras senectus varius quam nunc. Phasellus vulputate mi quam pharetra quis. Donec habitant nascetur enim ac, massa faucibus enim, non. </p>
+            <h3>How it works</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Commodo, fusce platea cras imperdiet tortor et. Pellentesque vitae sed molestie nunc cras senectus varius quam nunc. Phasellus vulputate mi quam pharetra quis. Donec habitant nascetur enim ac, massa faucibus enim, non. </p>
         </div>
     )
 }
