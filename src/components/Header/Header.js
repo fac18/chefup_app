d73e8ee5d934427fe012e600a222ff2830129220
Maria <m.olanipekun@hotmail.com>
2020-01-28 16:34:30 +0000

    made the header logo a link to home

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./header.css";
+import { Link } from 'react-router-dom';
 // import Logo from "../Logo/Logo";
 import { ReactComponent as Logo } from "../../icons/logoTextWhite.svg";
 
 const Header = () => {
   return (
     <div className="header">
-      <Logo />
+      <Logo> <Link to="/" /> </Logo>
     </div>
   );
 };
