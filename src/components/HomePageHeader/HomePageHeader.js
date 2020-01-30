66189aece16d6ffb10d097c22b99ef1709993635
Alexandra O'Mahony <alexandra.omahony1@gmail.com>
2020-01-30 12:33:15 +0000

    removed nav bar from homepage header and made logo responsive

--- src/components/HomePageHeader/HomePageHeader.js
+++ src/components/HomePageHeader/HomePageHeader.js
@@ -3,39 +3,46 @@ import "./homePageHeader.css";
 import { ReactComponent as HomePageLogo } from "../../icons/logoImageWhite.svg";
 import { Link } from "react-router-dom";
 import Search from "../Search/Search";
-import LoginModal from '../LoginModal/LoginModal'
+import LoginModal from "../LoginModal/LoginModal";
 
 const HomePageHeader = ({ searchInput, setSearchInput }) => {
   const [show, setShow] = React.useState(false);
-        
+
   const showModal = () => setShow(true);
   const hideModal = () => setShow(false);
-  
 
+  // for future iterations we would like to include a authorisation and create users
+  // <LoginModal show={show} handleClose={hideModal}>
+  //   <p>Modal</p>
+  //   <p>Data</p>
+  // </LoginModal>
+  // <nav>
+  //   <ul className="homePageNav">
+  //     <li>
+  //       <Link className="list" to="/About">
+  //         ABOUT US
+  //       </Link>
+  //     </li>
+  //     <li className="list" onClick={showModal}>
+  //       LOGIN
+  //     </li>
+  //     <li>
+  //       <Link className="signUp list" to="/SignUp">
+  //         SIGN UP
+  //       </Link>
+  //     </li>
+  //   </ul>
+  // </nav>
 
   return (
     <div className="homePageHeader">
       <div className="wrapper">
-        <LoginModal show={show} handleClose={hideModal}>
-          <p>Modal</p>
-          <p>Data</p>
-        </LoginModal>
-      <nav>
-        <ul className="homePageNav">
-          <li>
-            <Link className="list" to="/About">ABOUT US</Link>
-          </li>
-          <li className="list" onClick={showModal}>
-            LOGIN
-          </li>
-          <li>
-            <Link className="signUp list" to="/SignUp">SIGN UP</Link>
-          </li>
-        </ul>
-      </nav>
-      <div ><HomePageLogo className="logo"/></div>
-      <p className="tagLine"> Find a local chef and level up your cooking skills!</p>
-      <Search searchInput={searchInput} setSearchInput={setSearchInput} />
+        <HomePageLogo className="logo" />
+        <p className="tagLine">
+          {" "}
+          Find a local chef and level up your cooking skills!
+        </p>
+        <Search searchInput={searchInput} setSearchInput={setSearchInput} />
       </div>
     </div>
   );
