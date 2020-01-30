6f0d3cacf681c5f089203ddecd7454261fc311c6
Alexandra O'Mahony <alexandra.omahony1@gmail.com>
2020-01-30 15:49:34 +0000

    Changed expected text in test to match new icon's name

--- src/App.test.js
+++ src/App.test.js
@@ -9,17 +9,18 @@ test("full app rendering/navigating", () => {
   const history = createMemoryHistory();
   const token = process.env.REACT_APP_TOKEN;
 
-  if(token) {
-    
+  if (token) {
     const { container, getByText } = render(
       <Router history={history}>
         <App />
       </Router>
-    )
-    expect(container.textContent).toMatch('ABOUT USLOGINSIGN UPlogoImageWhite.svgFind a local chef and level up your cooking skills!homeIconWhite.svgteachIconWhite.svglearnIconWhite.svg')
-  
+    );
+    expect(container.textContent).toBe(
+      "logoImageWhite.svg Find a local chef and level up your cooking skills!Welcome to  logoTextBlack.svgLog inClosehomeIconWhite.svgteachIconWhite.svginfoIconWhite.svg"
+    );
+
     fireEvent.click(getByText(/about us/i));
-  
+
     // check that the content changed to the new page
     expect(container.textContent).toMatch(/about us/i);
   }
