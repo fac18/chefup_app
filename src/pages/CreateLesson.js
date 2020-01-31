f6d1f12117a3efebd746328de5a8130b8ff66ee5
nikkesan <nikke.san@gmail.com>
2020-01-31 12:35:40 +0000

    remove import aboutpage.css page not used

--- src/pages/CreateLesson.js
+++ src/pages/CreateLesson.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import Header from '../components/Header/Header';
-import Footer from '../components/Footer/Footer';
-import CreateNewLesson from '../components/CreateLesson/CreateNewLesson'
-import './pages_css/aboutpage.css';
-import './pages_css/createlesson.css';
+import React from "react";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+import CreateNewLesson from "../components/CreateLesson/CreateNewLesson";
+import "./pages_css/aboutpage.css";
 
 const CreateLesson = () => {
-    return (
-        <div className="bkg">
-            <Header />
-            <div className="container">
-            <h1 className="create-lesson-header">Create your own lesson</h1>
-            <CreateNewLesson />
-            </div>
-            <Footer />
-        </div>
-    )
-}
+  return (
+    <div className="bkg">
+      <Header />
+      <div className="container">
+        <h1 className="create-lesson-header">Create your own lesson</h1>
+        <CreateNewLesson />
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
-export default CreateLesson
+export default CreateLesson;
